9e12fc1904afb6cc1b94bfa93e2e44ea7fa3cdda
3urobeat <35304405+3urobeat@users.noreply.github.com>
2023-07-27 22:53:53 +0200

    Add Server DB and API calls with basic getters & setters
    Co-Authored-By: DerDeathraven <jean-luc@jlcd.de>

---
+++ composables/useDatabase.ts
@@ -0,0 +1,32 @@
+/*
+ * File: useDatabase.ts
+ * Project: bingo
+ * Created Date: 27.07.2023 19:32:28
+ * Author: 3urobeat
+ * 
+ * Last Modified: 27.07.2023 21:24:48
+ * Modified By: 3urobeat
+ * 
+ * Copyright (c) 2023 3urobeat <https://github.com/3urobeat>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+// Load database
+import nedb from "@seald-io/nedb";
+
+const database = new nedb<Player>({ filename: "./server/data/database.db", autoload: true }); // Autoload
+
+type Player = {
+    name: string,
+    lastActivity: number,
+    playfield: object
+};
+
+// Provide function to access Database instance
+export function useDatabase() {
+    return database;
+};
---
+++ server/api/set-name.ts
@@ -0,0 +1,34 @@
+/*
+ * File: set-name.ts
+ * Project: bingo
+ * Created Date: 27.07.2023 19:59:02
+ * Author: 3urobeat
+ * 
+ * Last Modified: 27.07.2023 21:26:19
+ * Modified By: 3urobeat
+ * 
+ * Copyright (c) 2023 3urobeat <https://github.com/3urobeat>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import { useDatabase } from "../../composables/useDatabase";
+
+
+// This function is executed when this API route is called
+export default defineEventHandler(async (event) => {
+    const db = useDatabase();
+
+    const params = await readBody(event);
+
+    if (params.name) {
+        db.updateAsync({ name: params.name }, { $set: { name: params.name, lastActivity: Date.now(), playfield: {} } }, { upsert: true });
+
+        return true;
+    }
+
+    return false;
+});
