7dd15edb902358b510b5edbad54e5a94f894a198
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-03-31 13:02:03 +0200

    refactor: Update page headers and simplify footer

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
  * Created Date: 2023-07-27 12:48:01
  * Author: 3urobeat
  *
- * Last Modified: 2024-03-25 17:41:06
+ * Last Modified: 2024-03-31 13:01:13
  * Modified By: 3urobeat
  *
  * Copyright (c) 2023 - 2024 3urobeat <https://github.com/3urobeat>
@@ -19,15 +19,7 @@
 
 
 <template>
-    <title>Play Bingo!</title>
-    <meta content="Play Bingo!" property="og:title" />
-    <meta content="Play Meme Bingo with your friends from any browser! A webgame made using Nuxt & Vue.js." property="og:description" />
-    <!-- <meta content="https://github.com/3urobeat/bingo" property="og:url" /> -->
-    <meta content="public/favicon.ico" property="og:image" /> <!-- TODO: Does this work? -->
-    <meta content="#43B581" data-react-helmet="true" name="theme-color" />
-
-
-    <div class="flex flex-col items-center justify-between h-screen w-screen overflow-y-hidden bg-gradient-to-br from-gray-400 via-gray-500 to-gray-600">
+    <div class="flex flex-col items-center justify-between h-screen w-screen overflow-auto bg-gradient-to-br from-gray-400 via-gray-500 to-gray-600">
         <nav class="flex justify-center">
             <div class="pt-2 w-28">
                 <img :src="logosrc" alt="Logo"/>
@@ -42,18 +34,22 @@
 
         <NuxtPage></NuxtPage>
 
-        <footer class="self-center flex flex-col items-center mb-2 text-xs md:text-sm text-gray-900">
+        <footer class="self-center flex flex-col items-center mb-2 mt-2 text-xs md:text-sm opacity-50 text-nowrap">
             Bingo v{{ packagejson.version }}
 
-            <button type="button" @click="clickGithub" class="mt-1 rounded-full px-2 text-gray-400 bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 text-center flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 bg-opacity-80">
+            <a class="flex w-fit items-center mt-0.5 -ml-1 rounded-full px-2 text-white bg-gray-700 hover:bg-gray-400 hover:transition-all bg-opacity-80" href="http://github.com/3urobeat/bingo" target="_blank">
+
+                <!-- GitHub logo -->
                 <svg class="mr-1" width="1em" height="1em" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
                 </svg>
-                <span class="text-gray-400 rounded-lg text-xm" href="https://github.com/3urobeat/bingo" target="_blank">Source Code</span>
-            </button>
 
-            <p>This application is licensed under the <a class="underline hover:text-gray-500 rounded-lg" href="https://www.gnu.org/licenses/" target="_blank">GPLv3 license</a></p>
-            <p>Copyright (c) 2023 <a class="underline hover:text-gray-500 rounded-lg" href="https://github.com/3urobeat" target="_blank">3urobeat</a></p>
+                <span class="text-gray-400 rounded-lg text-xm" href="https://github.com/3urobeat/bingo" target="_blank">Source Code</span>
+
+            </a>
+
+            <p>Licensed under <a class="underline hover:text-gray-500 rounded-lg" href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GPLv3</a></p>
+            <p>Copyright (c) 2023-2024 <a class="underline hover:text-gray-500 rounded-lg" href="https://github.com/3urobeat" target="_blank">3urobeat</a></p>
         </footer>
     </div>
 </template>
@@ -64,6 +60,17 @@
     import packagejson from "./package.json";
 
     const router = useRouter();
+
+
+    // Specify page information
+    useSeoMeta({
+        title: "Play Bingo!",
+        ogTitle: "Play Bingo!",
+        description: "Play Meme Bingo with your friends from any browser! A webgame made using Nuxt & Vue.js.",
+        ogDescription: "Play Meme Bingo with your friends from any browser! A webgame made using Nuxt & Vue.js.",
+        ogImage: "public/favicon.ico",
+        themeColor: "#43B581",
+    })
 
 
     /**
@@ -80,12 +87,4 @@
             router.push({ path: "/game/" + selectedName });
         }
     });
-
-    
-    /**
-     * Executed when user clicks the footer's GitHub icon
-     */
-    function clickGithub(event: Event) {
-        window.open('http://github.com/3urobeat/bingo', '_blank');
-    }
 </script>
